2aeb2ba6eb39ec3129c672a38e41d919b4a7ffa3
xtrp <36894883+xtrp@users.noreply.github.com>
2019-08-01 16:11:08 -0400

    finished error message boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-// remove preload css after window has loaded
+// add mobile class if mobile browser
 window.addEventListener("load", function(){
     // check for mobile browsers
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         document.body.classList.add("mobile");
     }
 
-    // check for unsupported browsers
-
+    // check for unsupported browsers (ES6 support)
+    try {
+        Function("() => {}");
+    } catch(error){
+        // ES6 is not available, so add error box
+        el.insertAdjacentHTML("afterbegin", '<div class="error_box"><strong>Browser not Supported</strong> &mdash; some features on this site may not work with your browser. Please switch to an alternative, like <a href="https://www.google.com/chrome/" target="_blank">Chrome</a>.</div>');
+    }
 });
