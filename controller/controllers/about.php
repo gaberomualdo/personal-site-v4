d80f5e3325044492a626e576a9e2269db7856b63
xtrp <36894883+xtrp@users.noreply.github.com>
2019-09-12 19:56:50 +0100

    moved api to subdomain, and updated readme to reflect changes

--- controller/controllers/about.php
+++ controller/controllers/about.php
@@ -1,2 +1,7 @@
 <?php
+$router->get("/about/", function(){
+    // get data from api/about/
+    $api_data = json_decode(file_get_contents("http://localhost:6000/api/about/"), true);
+    var_dump($api_data);
+});
 ?>
