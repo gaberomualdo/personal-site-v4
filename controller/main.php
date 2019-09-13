064e43d90fdbcec0c09c0fc3f052e4babfed4f2b
xtrp <36894883+xtrp@users.noreply.github.com>
2019-09-13 21:09:49 +0100

    added XML RSS Atom feed page at /feed/ route

--- controller/main.php
+++ controller/main.php
@@ -34,7 +34,7 @@ $routes = [
 foreach($routes as $route) {
     $router->get($route["route"], function() use( &$route ) {
         // get site details data from api/site_details/ and store in var
-        $site_details = json_decode(file_get_contents("https://api.xtrp.io/site_details"), true);
+        $site_details = json_decode(file_get_contents("https://api.xtrp.io/site_details/"), true);
             
         // get data from api/about/ and store in var
         $page_data = json_decode(file_get_contents($route["api_route"]), true);
@@ -43,6 +43,69 @@ foreach($routes as $route) {
         include_once "views/" . $route["view_filename"];
     });
 }
+
+// RSS feed
+$router->get("/feed/", function() {
+    // site details
+    $site_details = json_decode(file_get_contents("https://api.xtrp.io/site_details/"), true);
+    $site_title = $site_details["full_title"];
+    $site_author_name = $site_details["author"]["name"];
+
+    // all blog posts
+    $blog_posts = json_decode(file_get_contents("https://api.xtrp.io/blog/"), true);
+
+    // extra vars
+    $current_full_year = date("Y");
+    $latest_blog_post_datetime = date("c", strtotime($blog_posts[0]["last_updated"]));
+
+    // return RSS feed
+    header("Content-type: text/xml");
+
+    echo <<<EOT
+    
+    <?xml version='1.0' encoding='UTF-8'?>
+    <feed xmlns="http://www.w3.org/2005/Atom">
+        <link href="/feed/" rel="self" type="application/atom+xml"/>
+        <link href="/" rel="alternate" type="text/html"/>
+        <id>/</id>
+        <title type="html">{$site_title}</title>
+        <updated>{$latest_blog_post_datetime}</updated>
+
+        <author>
+            <name>{$site_author_name}</name>
+        </author>
+
+        <rights> (c) {$current_full_year} {$site_author_name} </rights>
+    EOT;
+
+    // loop through posts and add RSS
+    foreach($blog_posts as $post) {
+        $post_title = $post["title"];
+        $post_url = "/blog/" . str_replace("-", "/", $post["last_updated"]) . "/" . $post["filename"] . "/";
+        $post_content = $post["content"];
+        $post_datetime = date("c", strtotime($post["last_updated"]));
+
+        echo <<<EOT
+        <entry>
+            <title type="html">{$post["title"]}</title>
+            <link href="{$post_url}" rel="alternate" type="text/html" title="{$post_title}"/>
+            <published>{$post_datetime}</published>
+            <id>{$post_url}/id>
+            
+            <content type="html" xml:base="{$post_url}"> {$post_content} </content>
+            
+            <author>
+                <name>{$site_author_name}</name>
+            </author>
+        </entry>
+        EOT;
+    }
+    
+    // end of feed
+    echo "</feed>";
+
+    // yay!
+});
 
 // errors
 $router->error("method_not_allowed", function() {
