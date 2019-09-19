e5271b243734aa936fd0ff0e4e6f73b16d691637
xtrp <36894883+xtrp@users.noreply.github.com>
2019-09-19 19:00:30 +0100

    testing updates

---
+++ views/assets/html/nav.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Site Details</h1><code>" . $site_details . "</code>";
+echo "<h1>Page Details</h1><code>" . $page_details . "</code>";
+echo "<h1>Page Data</h1><code>" . $page_data . "</code>";
+echo "<h1>Filename</h1><code>" . $filename . "</code>";
+?>
