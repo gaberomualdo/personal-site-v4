1312b106501834aff63661d7a4a1d00dfcb682f6
xtrp <36894883+xtrp@users.noreply.github.com>
2019-09-19 20:41:19 +0100

    testts

--- views/feed.php
+++ views/feed.php
@@ -12,10 +12,7 @@ $current_full_year = date("Y");
 $latest_blog_post_datetime = "2019-09-18";
 
 // set latest time if possible
-
-if($page_data["blog_posts"] && count($page_data["blog_posts"]) > 0){
-   $latest_blog_post_datetime = date("c", strtotime($page_data["blog_posts"][0]["last_updated"]));
-}
+$latest_blog_post_datetime = date("c", strtotime($page_data["blog_posts"][0]["last_updated"]));
 
 $site_title = $site_details["full_title"];
 $site_author_name = $site_details["author"]["name"];
@@ -38,29 +35,27 @@ echo <<<EOT
     <rights> (c) {$current_full_year} {$site_author_name} </rights>
 EOT;
 
-if($page_data["blog_posts"] && count($page_data["blog_posts"]) > 0) {
-    // loop through posts and add RSS
-    foreach($page_data["blog_posts"] as $post) {
-        $post_title = $post["title"];
-        $post_url = "/blog/" . str_replace("-", "/", $post["last_updated"]) . "/" . $post["filename"] . "/";
-        $post_content = html_entity_decode($post["content"], null, "UTF-8");
-        $post_datetime = date("c", strtotime($post["last_updated"]));
+// loop through posts and add RSS
+foreach($page_data["blog_posts"] as $post) {
+    $post_title = $post["title"];
+    $post_url = "/blog/" . str_replace("-", "/", $post["last_updated"]) . "/" . $post["filename"] . "/";
+    $post_content = html_entity_decode($post["content"], null, "UTF-8");
+    $post_datetime = date("c", strtotime($post["last_updated"]));
 
-        echo <<<EOT
-        <entry>
-            <title>{$post_title}</title>
-            <link href="{$post_url}" rel="alternate" type="text/html" title="{$post_title}" />
-            <published>{$post_datetime}</published>
-            <id>{$post_url}</id>
-            
-            <content type="html" xml:base="{$post_url}"> {$post_content} </content>
-            
-            <author>
-                <name>{$site_author_name}</name>
-            </author>
-        </entry>
-        EOT;
-    }
+    echo <<<EOT
+    <entry>
+        <title>{$post_title}</title>
+        <link href="{$post_url}" rel="alternate" type="text/html" title="{$post_title}" />
+        <published>{$post_datetime}</published>
+        <id>{$post_url}</id>
+        
+        <content type="html" xml:base="{$post_url}"> {$post_content} </content>
+        
+        <author>
+            <name>{$site_author_name}</name>
+        </author>
+    </entry>
+    EOT;
 }
 
 // end of feed
