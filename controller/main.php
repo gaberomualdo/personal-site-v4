09926b811db7e780f40ebde893040fa191b5c62b
xtrp <36894883+xtrp@users.noreply.github.com>
2019-09-19 20:41:51 +0100

    temporarily added back errors

--- controller/main.php
+++ controller/main.php
@@ -1,6 +1,11 @@
 <?php
 // this file has controllers for the site
 
+// display errors, VERY IMPORTANT
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // include api
 include_once __DIR__ . "/../api/main.php";
 
