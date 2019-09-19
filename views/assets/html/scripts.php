46520eaa4746e0b322168c36011d4bc756801e34
xtrp <36894883+xtrp@users.noreply.github.com>
2019-09-19 20:55:10 +0100

    moved tests to scripts partial

--- views/assets/html/scripts.php
+++ views/assets/html/scripts.php
@@ -1,2 +1,22 @@
 <script src="/views/assets/js/script.js"></script>
 <script src="/views/assets/js/pages/<?=$filename ?>.js"></script>
+
+<script>
+
+echo "console.log('site_details');console.log(JSON.parse(`";
+var_dump($site_details);
+echo "`));";
+
+echo "console.log('page_details');console.log(JSON.parse(`";
+var_dump($page_details);
+echo "`));";
+
+echo "console.log('page_data');console.log(JSON.parse(`";
+var_dump($page_data);
+echo "`));";
+
+echo "console.log('filename');console.log(JSON.parse(`";
+var_dump($filename);
+echo "`));";
+
+</script>
