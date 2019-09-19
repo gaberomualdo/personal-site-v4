4fae07c1e18a49f89d907489178a231fa5d685c8
xtrp <36894883+xtrp@users.noreply.github.com>
2019-09-19 19:01:42 +0100

    more testing updates

--- views/assets/html/nav.php
+++ views/assets/html/nav.php
@@ -1,6 +1,17 @@
 <?php
-echo "<h1>Site Details</h1><code>" . $site_details . "</code>";
-echo "<h1>Page Details</h1><code>" . $page_details . "</code>";
-echo "<h1>Page Data</h1><code>" . $page_data . "</code>";
-echo "<h1>Filename</h1><code>" . $filename . "</code>";
+echo "<h1>Site Details</h1><code>";
+var_dump($site_details);
+echo "</code>";
+
+echo "<h1>Page Details</h1><code>";
+var_dump($page_details);
+echo "</code>";
+
+echo "<h1>Page Data</h1><code>";
+var_dump($page_data);
+echo "</code>";
+
+echo "<h1>Filename</h1><code>";
+var_dump($filename);
+echo "</code>";
 ?>
