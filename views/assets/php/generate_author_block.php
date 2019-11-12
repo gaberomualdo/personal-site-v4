fa276bd3904c5d80a6ad5afd4156cf9e6c228ccb
xtrp <36894883+xtrp@users.noreply.github.com>
2019-11-12 16:38:43 +0000

    add author block

--- views/assets/php/generate_author_block.php
+++ views/assets/php/generate_author_block.php
@@ -2,11 +2,28 @@
 
 function generate_author_block() {
     global $page_data;
+    global $site_data;
+
+    $social_links_HTML = "";
+    foreach($site_details["author"]["social_urls"] as $social_url) {
+        $social_links_HTML .= "<a href='" . $social_url["url"] . "' target='_blank'>" . $social_url["svg"] . "</a>";
+    }
 
     return '
     <div class="side_block more_posts_block">
         <div class="content">
+            <div class="top_area">
+                <img class="left" alt="Fred Adams Picture" src="/api/content/static_files/profile_picture.png">
+                <div class="right">
+                    <h1>Fred Adams</h1>
+                    <div class="social_links">
+                        ' . $social_links_HTML . '
+                    </div>
+                </div>
+            </div>
+            <p>
             
+            </p>
         </div>
     </div>
     ';
