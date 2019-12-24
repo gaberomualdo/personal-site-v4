9d1c953f8c7a66f15b667d6e83e0ddcbf5966f00
xtrp <36894883+xtrp@users.noreply.github.com>
2019-12-24 11:55:23 +0000

    uncache css

--- views/assets/html/head.php
+++ views/assets/html/head.php
@@ -102,15 +102,15 @@
     <meta name="theme-color" content="#ffffff">
 
     <?php // skeleton stylesheets ?>
-    <link rel="stylesheet" href="/views/resources/skeleton/normalize.min.css?v=6">
-    <link rel="stylesheet" href="/views/resources/skeleton/skeleton.min.css?v=6">
+    <link rel="stylesheet" href="/views/resources/skeleton/normalize.min.css?v=10">
+    <link rel="stylesheet" href="/views/resources/skeleton/skeleton.min.css?v=10">
     
     <?php // finally, CSS! ?>
-    <link rel="stylesheet" href="/views/assets/css/main.css?v=8">
+    <link rel="stylesheet" href="/views/assets/css/main.css?v=10">
     <?php
     // only add page js if exists
     if(in_array($filename . ".css", scandir(dirname(__FILE__) . "/../css/pages/"))) {
-        echo '<link rel="stylesheet" href="/views/assets/css/pages/' . $filename . '.css?v=8">';
+        echo '<link rel="stylesheet" href="/views/assets/css/pages/' . $filename . '.css?v=10">';
     }
     ?>
 </head>
