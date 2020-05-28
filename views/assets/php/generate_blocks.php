d1bc79208bdbea6573e35d08df70e40aa0493640
xtrp <36894883+xtrp@users.noreply.github.com>
2020-05-28 13:58:34 +0100

    Rename generate_blocks dir to just blocks

--- views/assets/php/generate_blocks.php
+++ views/assets/php/generate_blocks.php
@@ -1,21 +1,21 @@
 <?php
 // include necessary block
-include "generate_blocks_dependencies/generate_blog_block.php";
-include "generate_blocks_dependencies/generate_code_block.php";
+include "blocks/generate_blog_block.php";
+include "blocks/generate_code_block.php";
 
-include "generate_blocks_dependencies/generate_more_from_fred_block.php";
-include "generate_blocks_dependencies/generate_author_block.php";
-include "generate_blocks_dependencies/generate_share_post_block.php";
-include "generate_blocks_dependencies/generate_another_post_block.php";
+include "blocks/generate_more_from_fred_block.php";
+include "blocks/generate_author_block.php";
+include "blocks/generate_share_post_block.php";
+include "blocks/generate_another_post_block.php";
 
 /*
 @TODO implement:
-include "generate_blocks_dependencies/generate_featured_project_block.php";
+include "blocks/generate_featured_project_block.php";
 */
 
 /*
 @REMOVED
-include "generate_blocks_dependencies/generate_on_this_site_block.php";
+include "blocks/generate_on_this_site_block.php";
 */
 
 ?>
