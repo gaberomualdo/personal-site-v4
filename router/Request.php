3c43d2dcdd26393e42be7ef3a4d8639e8a7c4899
xtrp <36894883+xtrp@users.noreply.github.com>
2020-09-12 12:10:43 -0400

    Add query param support and support for lack of / at the end of request URLs

--- router/Request.php
+++ router/Request.php
@@ -7,12 +7,38 @@ This is the Request class. It gets the current request associated with the serve
 class Request {
 	// constructor: get server variables and save to class, and split request uri into array of directories
 	public function __construct() {
+		function endsWith( $haystack, $needle ) {
+			$length = strlen( $needle );
+			if( !$length ) {
+				return true;
+			}
+			return substr( $haystack, -$length ) === $needle;
+		} // from https://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+
 		// variable for server vars to save in class
-		$serverVariablesToSave = array("REQUEST_URI", "REQUEST_METHOD", "REQUEST_TIME", "SERVER_NAME", "HTTP_USER_AGENT");
+		$serverVariablesToSave = array("REQUEST_METHOD", "REQUEST_TIME", "SERVER_NAME", "HTTP_USER_AGENT");
 		
 		// loop through each server variable to save and store value in class with corresponding key
 		foreach($serverVariablesToSave as $variable) {
 			$this->$variable = $_SERVER[$variable];
+		}
+
+		// useful vars
+		$url = ("https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
+		$parsed_url = parse_url($url);
+		
+		// request URI has '/' at the end and does not include query params
+		if(endsWith($parsed_url['path'], '/')) {
+			$this->REQUEST_URI = $parsed_url['path'];
+		} else {
+			$this->REQUEST_URI = $parsed_url['path'] . "/";
+		}
+
+		// add query params variable to class
+		if(array_key_exists('query', $parsed_url)) {
+			parse_str($parsed_url['query'], $this->QUERY_PARAMS);
+		} else {
+			$this->QUERY_PARAMS = [];
 		}
 
 		// split URI into directories, and store in variable
@@ -20,6 +46,9 @@ class Request {
 	}
 
 	// getter methods for variables
+	public function getQueryParams() {
+		return $this->QUERY_PARAMS;
+	}
 	public function getRequestURI() {
 		return $this->REQUEST_URI;
 	}
